b362683412fcff942fb6ddbd9f32dab9d60ac094
Shane Phillips <shane@loongphillips.com>
2024-01-30 13:14:40 -0500

    innerHTML p addition

--- Inner HTML/index.js
+++ Inner HTML/index.js
@@ -1,3 +1,9 @@
 const container =
 	document.getElementById("container");
 container.innerHTML = "<button>Buy!</button>";
+container.innerHTML =
+	"<button onclick='buy()'>Buy!</button>";
+const buy = () => {
+	container.innerHTML +=
+		"<p>Thanks for buying!</p>";
+};
