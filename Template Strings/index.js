6030185f62471b1d7aebb0ac43015c1dad818f73
Shane Phillips <shane@loongphillips.com>
2024-01-30 13:59:25 -0500

    multiple lines

--- Template Strings/index.js
+++ Template Strings/index.js
@@ -1,12 +1,10 @@
 // template strings/literals
 
-const recipient = "James";
+const recipient = "My Dude";
+const sender = "Shane";
 
-// Refactor the email string to use template strings
-const email =
-	// "Hey " +
-	// recipient +
-	// "! How is it going? Cheers Per";
-	`Hey ${recipient}, how's it going? Cheers, Per.`;
+const email = `Hey ${recipient},
+how's it going?
+Cheers, ${sender}.`;
 
 console.log(email);
