677fb4173c22c9e7fcdf76e3b18a6a0d19d98e0c
Shane Phillips <shane@loongphillips.com>
2024-03-07 15:03:45 -0500

    truthy falsy

---
+++ Truthy & Falsy/index.js
@@ -0,0 +1,17 @@
+const credits = 0;
+
+if (credits) {
+	console.log("Let's play! 🎰");
+} else {
+	console.log("Sorry, you have no credits! 😭");
+}
+
+//////
+
+let currentViewers = null;
+currentViewers = ["Jane", "John"];
+// currentViewers = null;
+
+if (currentViewers) {
+	console.log("Viewers!");
+}
